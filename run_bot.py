7fd2494dfb4f7eec52553372f3b367bee5cef767
LeCodeurDu78 <adamrespeliers@gmail.com>
2023-09-29 12:30:46 +0200

    Creation fichier Requirments.txt

--- run_bot.py
+++ run_bot.py
@@ -1,42 +1,31 @@
-import os
+from os import remove
 from upload import upload
 from montage import montage
 from download import download
 from authenticate import authenticate
-from PyQt5.QtGui import QStandardItem
 
-if:
+if __name__ == "__main__":
     api_number = 2
     nb_videos = 5
 
     for i in range(api_number):
         youtube = authenticate(i)
-        titles, secondsVideos = download(youtube, nb_videos, i, list_view)
-        
-        item = QStandardItem("\nDownload fini\n\n")
-        list_view.appendRow(item)
+        titles, secondsVideos = download(youtube, nb_videos, i)
+        print("\nDownload fini\n\n")
         
         try:
             montage(nb_videos, secondsVideos, titles)
-            
-            item = QStandardItem("Montage fini\n\n")
-            list_view.appendRow(item)
+            print("Montage fini\n\n")
         except: 
             montage(nb_videos, secondsVideos, titles)
-            
-            item = QStandardItem("Montage fini\n\n")
-            list_view.appendRow(item)
+            print("Montage fini\n\n")
         try: 
-            upload(youtube, titles, nb_videos, list_view)
-
-            item = QStandardItem("Upload fini")
-            list_view.appendRow(item)
+            upload(youtube, titles, nb_videos)
+            print("Upload fini")
         except:
-            upload(youtube, titles, nb_videos, list_view)
-            
-            item = QStandardItem("Upload fini")
-            list_view.appendRow(item)
+            upload(youtube, titles, nb_videos)
+            print("Upload fini")
 
         for i in range(len(titles)):
-            os.remove(f"Videos/{titles[i]}.mp4") 
-            os.remove(f"Videos/youtube{i}_all.mp4")
+            remove(f"Videos/{titles[i]}.mp4") 
+            remove(f"Videos/youtube{i}_all.mp4")
